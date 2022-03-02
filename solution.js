f208c763c7de6f6aa2f970086bea5b842eba5404
Nick Lopez <nicklopez5454@yahoo.com>
2022-03-02 16:03:36 -0600

    Had to refork and copy file because pull requests werent working

--- solution.js
+++ solution.js
@@ -1,6 +1,11 @@
 // You must RETURN your result in each function!
 
 function printNums(nums) {
+  let newArr = []
+  nums.forEach((number) => {
+    newArr.push(number)
+  })
+  return newArr
   /**
    *  Using .forEach, add each number in the nums array to a new array.
    * @param nums => [1,2,3,4]
@@ -8,6 +13,12 @@ function printNums(nums) {
 }
 
 function returnSums(nums) {
+  let newArr = []
+  nums.forEach((number, index) => {
+    newArr.push(number + index)
+  })
+
+  return newArr
   /**
    * Using forEach, push the sum of n plus the index
    * to a new array and return the new array
@@ -16,6 +27,11 @@ function returnSums(nums) {
 }
 
 function returnTotal(objs) {
+  let acc = 0
+  objs.forEach((element) => {
+    acc += element.n
+  })
+  return acc
   /**
    * Using forEach return a number that is the total sum of all numbers in the array of objects.
    * The key for each object will be n
@@ -24,6 +40,12 @@ function returnTotal(objs) {
 }
 
 function printMoney(decimals) {
+  let newArr = decimals.map((element) => {
+    let newElem = '$' + element.toFixed(2)
+    return newElem
+  })
+  return newArr
+
   /**
    * @param decimals => [2.30, 2.40, 32.99]
    * Using .map, return an array of decimals formatted as dollars.
@@ -36,6 +58,8 @@ function printMoney(decimals) {
 }
 
 function returnAllTrueValues(values) {
+  return values.filter((obj) => obj.happy === true)
+
   /**
    * @param values => [{name: 'Joe', happy:false}]
    * Using .filter, return an array of objects where happy equals true
@@ -43,6 +67,9 @@ function returnAllTrueValues(values) {
 }
 
 function addTwo(nums) {
+  return nums.reduce((accumulator, value) => {
+    return accumulator + value + 2
+  }, 0)
   /**
    * @param nums => [1,2,3,4]
    * Using .reduce, return the total sum of all numbers.
@@ -53,6 +80,9 @@ function addTwo(nums) {
 }
 
 function joinStrings(strings) {
+  return strings.reduce((accumulator, value) => {
+    return accumulator + value
+  }, '')
   /**
    * @param strings => ['g', 'f', 'z']
    * Using .reduce, return a new string containing each string from the strings array.
@@ -61,6 +91,9 @@ function joinStrings(strings) {
 
 //Bonus
 function sortObjectsByValue(objs) {
+  return objs.sort((a, b) => {
+    return a.value - b.value
+  })
   /**
    * @param objs => [{value: 1}, {value:2}, {value:23}]
    * Using .sort, sort the array of objects by the value key in ascending order
