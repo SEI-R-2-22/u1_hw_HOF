e20cad8cefc91c84306248566906416df8fd5b2c
Brad Lewis <BLewis739@gmail.com>
2022-03-02 16:47:52 -0500

    Completed all problems

--- solution.js
+++ solution.js
@@ -5,6 +5,11 @@ function printNums(nums) {
    *  Using .forEach, add each number in the nums array to a new array.
    * @param nums => [1,2,3,4]
    */
+  const newArr = []
+  nums.forEach((element) => {
+    newArr.push(element)
+  })
+  return newArr
 }
 
 function returnSums(nums) {
@@ -13,6 +18,11 @@ function returnSums(nums) {
    * to a new array and return the new array
    * @param nums => [1,2,3,4,5]
    */
+  const newArr = []
+  nums.forEach((element, ind) => {
+    newArr.push(element + ind)
+  })
+  return newArr
 }
 
 function returnTotal(objs) {
@@ -21,6 +31,11 @@ function returnTotal(objs) {
    * The key for each object will be n
    * @param objs => [{n:1}]
    */
+  total = 0
+  objs.forEach((num) => {
+    total += num.n
+  })
+  return total
 }
 
 function printMoney(decimals) {
@@ -33,6 +48,10 @@ function printMoney(decimals) {
    * }
    * Hint: Not all decimals have two places, make sure to return each decimal with the proper decimal places. .toFixed may be useful :)
    */
+  const newArr = decimals.map((num) => {
+    return '$' + num.toFixed(2)
+  })
+  return newArr
 }
 
 function returnAllTrueValues(values) {
@@ -40,6 +59,10 @@ function returnAllTrueValues(values) {
    * @param values => [{name: 'Joe', happy:false}]
    * Using .filter, return an array of objects where happy equals true
    */
+  const newArr = values.filter((item) => {
+    return item.happy
+  })
+  return newArr
 }
 
 function addTwo(nums) {
@@ -50,6 +73,10 @@ function addTwo(nums) {
    */
   //   3
   //   4 => 7
+  const twoSum = nums.reduce((acc, val) => {
+    return acc + val + 2
+  }, 0)
+  return twoSum
 }
 
 function joinStrings(strings) {
@@ -57,6 +84,10 @@ function joinStrings(strings) {
    * @param strings => ['g', 'f', 'z']
    * Using .reduce, return a new string containing each string from the strings array.
    */
+  const newStr = strings.reduce((acc, val) => {
+    return acc + val
+  })
+  return newStr
 }
 
 //Bonus
@@ -66,6 +97,10 @@ function sortObjectsByValue(objs) {
    * Using .sort, sort the array of objects by the value key in ascending order
    * .sort documentation https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
    */
+  objs.sort((a, b) => a.value - b.value)
+  return objs
+
+  // Note from BL - Help received by https://simplernerd.com/js-sort-array-based-on-object-key/
 }
 
 module.exports = {
