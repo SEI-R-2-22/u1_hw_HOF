dec3de3fa42e81e1311f21d6ad4e525bbb9fb0f8
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-03-02 22:42:33 -0500

    completed problems 1-7

--- solution.js
+++ solution.js
@@ -1,21 +1,36 @@
 // You must RETURN your result in each function!
 
 function printNums(nums) {
+  let addEm = []
+  nums.forEach((num) => {
+    addEm.push(num)
+  })
+  return addEm
+}
+
+function returnSums(nums) {
+  let newArray = []
+  nums.forEach((num, index) => {
+    newArray.push((num += index))
+  })
+  return newArray
   /**
    *  Using .forEach, add each number in the nums array to a new array.
    * @param nums => [1,2,3,4]
    */
 }
-
-function returnSums(nums) {
-  /**
-   * Using forEach, push the sum of n plus the index
-   * to a new array and return the new array
-   * @param nums => [1,2,3,4,5]
-   */
-}
+/**
+ * Using forEach, push the sum of n plus the index
+ * to a new array and return the new array
+ * @param nums => [1,2,3,4,5]
+ */
 
 function returnTotal(objs) {
+  let sum = 0
+  objs.forEach((num) => {
+    sum += num.n
+  })
+  return sum
   /**
    * Using forEach return a number that is the total sum of all numbers in the array of objects.
    * The key for each object will be n
@@ -24,6 +39,11 @@ function returnTotal(objs) {
 }
 
 function printMoney(decimals) {
+  const arrDecimals = decimals.map((decimals) => {
+    const newArray = '$' + decimals.toFixed(2)
+    return newArray
+  })
+  return arrDecimals
   /**
    * @param decimals => [2.30, 2.40, 32.99]
    * Using .map, return an array of decimals formatted as dollars.
@@ -40,6 +60,10 @@ function returnAllTrueValues(values) {
    * @param values => [{name: 'Joe', happy:false}]
    * Using .filter, return an array of objects where happy equals true
    */
+  const array = values.filter((num) => {
+    return num.happy === true
+  })
+  return array
 }
 
 function addTwo(nums) {
@@ -50,9 +74,17 @@ function addTwo(nums) {
    */
   //   3
   //   4 => 7
+  const addTwoo = nums.reduce((acc, value) => {
+    return acc + 2 + value
+  }, 0)
+  return addTwoo
 }
 
 function joinStrings(strings) {
+  let strings2 = strings.reduce((acc, value) => {
+    return acc + value
+  })
+  return strings2
   /**
    * @param strings => ['g', 'f', 'z']
    * Using .reduce, return a new string containing each string from the strings array.
