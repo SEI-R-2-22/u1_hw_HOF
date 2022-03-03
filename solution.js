3dce0ae8657f18201b2359178f59cfbe141b8fc6
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-03-02 20:57:59 -0600

    Completed!

--- solution.js
+++ solution.js
@@ -5,6 +5,10 @@ function printNums(nums) {
    *  Using .forEach, add each number in the nums array to a new array.
    * @param nums => [1,2,3,4]
    */
+  const result = []
+  nums.forEach(element => result.push(element))
+  return result
+  
 }
 
 function returnSums(nums) {
@@ -13,6 +17,9 @@ function returnSums(nums) {
    * to a new array and return the new array
    * @param nums => [1,2,3,4,5]
    */
+   const result = []
+  nums.forEach((element, idx) => result.push(element + idx))
+  return result
 }
 
 function returnTotal(objs) {
@@ -21,6 +28,9 @@ function returnTotal(objs) {
    * The key for each object will be n
    * @param objs => [{n:1}]
    */
+  let total = 0
+  objs.forEach((element) => total += element.n)
+  return total
 }
 
 function printMoney(decimals) {
@@ -33,6 +43,7 @@ function printMoney(decimals) {
    * }
    * Hint: Not all decimals have two places, make sure to return each decimal with the proper decimal places. .toFixed may be useful :)
    */
+  return decimals.map(element => '$' + element.toFixed(2))
 }
 
 function returnAllTrueValues(values) {
@@ -40,6 +51,7 @@ function returnAllTrueValues(values) {
    * @param values => [{name: 'Joe', happy:false}]
    * Using .filter, return an array of objects where happy equals true
    */
+  return values.filter(element => element.happy)
 }
 
 function addTwo(nums) {
@@ -50,6 +62,7 @@ function addTwo(nums) {
    */
   //   3
   //   4 => 7
+  return nums.reduce((acc, element) => acc += element + 2,0)
 }
 
 function joinStrings(strings) {
@@ -57,6 +70,7 @@ function joinStrings(strings) {
    * @param strings => ['g', 'f', 'z']
    * Using .reduce, return a new string containing each string from the strings array.
    */
+  return strings.reduce((str, element) => str += element, '')
 }
 
 //Bonus
@@ -66,6 +80,7 @@ function sortObjectsByValue(objs) {
    * Using .sort, sort the array of objects by the value key in ascending order
    * .sort documentation https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
    */
+  return objs.sort((a, b) => a.value-b.value)
 }
 
 module.exports = {
