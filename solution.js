ce6ff1d7975d0a4e217681c6c02df4411c500a2e
tiffpereira <tiffanympereira@gmail.com>
2022-03-03 08:56:04 -0500

    final commit

--- solution.js
+++ solution.js
@@ -1,29 +1,38 @@
 // You must RETURN your result in each function!
 
-function printNums(nums) {
+const numbers = [1, 2, 3, 4]
+
+ numbers.forEach((nums) => {
+  return nums
   /**
    *  Using .forEach, add each number in the nums array to a new array.
    * @param nums => [1,2,3,4]
    */
-}
+})
+// console.log(numbers)
 
-function returnSums(nums) {
+const returnSums = [1, 2, 3, 4, 5]
+ returnSums.forEach((a, b) => {
+  return sum(a + b)
   /**
    * Using forEach, push the sum of n plus the index
    * to a new array and return the new array
    * @param nums => [1,2,3,4,5]
    */
-}
-
-function returnTotal(objs) {
+})
+const returnTotal = [{n:1}]
+returnTotal.forEach((objs) => {
+  return sum{objs: objs:a + objs:b}
   /**
    * Using forEach return a number that is the total sum of all numbers in the array of objects.
    * The key for each object will be n
    * @param objs => [{n:1}]
    */
-}
+})
 
-function printMoney(decimals) {
+decimals = [2.30, 2,40, 32.99]
+const printMoney = decimals.map((decimal) => {
+  return (Math.round(decimals * 100 / 100).toFixed(2))
   /**
    * @param decimals => [2.30, 2.40, 32.99]
    * Using .map, return an array of decimals formatted as dollars.
@@ -33,16 +42,23 @@ function printMoney(decimals) {
    * }
    * Hint: Not all decimals have two places, make sure to return each decimal with the proper decimal places. .toFixed may be useful :)
    */
-}
+})
+// console.log(printMoney)
 
-function returnAllTrueValues(values) {
+const arrNames = [{name: 'Joe', happy:false}]
+const returnAllTrueValues = arrNames.filter((values) => {
+  return values.length > 3
   /**
    * @param values => [{name: 'Joe', happy:false}]
    * Using .filter, return an array of objects where happy equals true
    */
-}
+})
+console.log(returnAllTrueValues)
 
-function addTwo(nums) {
+
+const arrNums= [1, 2, 3, 4]
+const  addTwo = arrNums.reduce((nums) => {
+  return sum(nums + 2)
   /**
    * @param nums => [1,2,3,4]
    * Using .reduce, return the total sum of all numbers.
@@ -50,14 +66,18 @@ function addTwo(nums) {
    */
   //   3
   //   4 => 7
-}
+})
 
-function joinStrings(strings) {
+
+arrStrings = ['g', 'f', 'z']
+const joinStrings = arrStrings.reduce((strings) => {
+    return arrStrings.toString()
+
   /**
    * @param strings => ['g', 'f', 'z']
    * Using .reduce, return a new string containing each string from the strings array.
    */
-}
+})
 
 //Bonus
 function sortObjectsByValue(objs) {
@@ -68,13 +88,13 @@ function sortObjectsByValue(objs) {
    */
 }
 
-module.exports = {
-  printNums,
-  returnSums,
-  returnTotal,
-  printMoney,
-  returnAllTrueValues,
-  sortObjectsByValue,
-  addTwo,
-  joinStrings
-}
+// module.exports = {
+//   printNums,
+//   returnSums,
+//   returnTotal,
+//   printMoney,
+//   returnAllTrueValues,
+//   sortObjectsByValue,
+//   addTwo,
+//   joinStrings
+// }
