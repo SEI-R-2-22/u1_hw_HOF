e320cae8ab09e7022f1f29566329627c58d31958
Aqsa Saleheene <angieclrk21@gmail.com>
2022-03-02 20:06:52 -0500

    hw

--- solution.js
+++ solution.js
@@ -1,6 +1,15 @@
 // You must RETURN your result in each function!
 
 function printNums(nums) {
+
+  const array = []
+  numbers.forEach((el) => {
+    array.push(el)
+  })
+  return array 
+{
+}
+///////////////////////////////////////////////////////////////////////
   /**
    *  Using .forEach, add each number in the nums array to a new array.
    * @param nums => [1,2,3,4]
@@ -8,22 +17,60 @@ function printNums(nums) {
 }
 
 function returnSums(nums) {
+  const array = []
+  numbers.forEach((n, index) => {
+    array.push(num + index)
+  })
+  return array
+  
+  ////////////////////////////////////////////////////////////////
   /**
    * Using forEach, push the sum of n plus the index
    * to a new array and return the new array
    * @param nums => [1,2,3,4,5]
    */
 }
+let numbers2 = []
+ numbers2.forEach( (n,index) => {
+  numbers2.push(n + index)
+})
+const obj1 = {
+  num: 1
+}
+
+  const obj2 = {
+    num: 2
+  }
+
+    const obj3 = {
+      num: 3
+  
+}
+const objArray = [obj1, obj2, obj3]
 
 function returnTotal(objs) {
+  ////////////////////////////////////////////////////////////////////
   /**
    * Using forEach return a number that is the total sum of all numbers in the array of objects.
    * The key for each object will be n
    * @param objs => [{n:1}]
    */
+  let newNumber = 0;
+  objs.reduce((number, value) => {
+return number + value.n
+  } ).forEach((numbers) => {
+    newNumber.push(numbers)
+  })
+return newNumber
 }
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 function printMoney(decimals) {
+  let money = decimals.map((numbers) => {
+  return 'money' + numbers.toFixed(2)
+  })
+  return money
+
   /**
    * @param decimals => [2.30, 2.40, 32.99]
    * Using .map, return an array of decimals formatted as dollars.
@@ -34,8 +81,13 @@ function printMoney(decimals) {
    * Hint: Not all decimals have two places, make sure to return each decimal with the proper decimal places. .toFixed may be useful :)
    */
 }
+// pull array using map function 
+// arrange array when map
 
 function returnAllTrueValues(values) {
+ const happy = values.fliter((feeling) => feeling.happy === true)
+ return feeling
+ ////////////////////////////////////////////////////////////////////////
   /**
    * @param values => [{name: 'Joe', happy:false}]
    * Using .filter, return an array of objects where happy equals true
@@ -43,6 +95,9 @@ function returnAllTrueValues(values) {
 }
 
 function addTwo(nums) {
+  
+ return numbers.reduce((a,b) => a+b+2,0)
+
   /**
    * @param nums => [1,2,3,4]
    * Using .reduce, return the total sum of all numbers.
@@ -51,16 +106,26 @@ function addTwo(nums) {
   //   3
   //   4 => 7
 }
-
+///////////////////////////////////////////////////////////////////////
 function joinStrings(strings) {
+  const words = strings.reduce ((acc,string)=> {
+    return accumulator && string
+  })
+  return strings & words 
   /**
    * @param strings => ['g', 'f', 'z']
    * Using .reduce, return a new string containing each string from the strings array.
    */
 }
-
+///////////////////////////////////////////////////////////////////////////
 //Bonus
 function sortObjectsByValue(objs) {
+  const sort = obj.sort(function (obj1,obj2) {
+return  obj1.val - obj2.val
+  })
+  return sort
+
+
   /**
    * @param objs => [{value: 1}, {value:2}, {value:23}]
    * Using .sort, sort the array of objects by the value key in ascending order
