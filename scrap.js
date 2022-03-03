77e68bd495bff962e8312d3f094ec33379903183
rarebit3 <johntrevorbond@gmail.com>
2022-03-03 11:53:10 -0500

    homework complete

---
+++ scrap.js
@@ -0,0 +1,10 @@
+value = {name: 'Joe', happy:false}
+    /**
+     * @param values => [{name: 'Joe', happy:false}]
+     * Using .filter, return an array of objects where happy equals true
+     */
+    //  const result = values.filter(mood => mood.happy === true);
+    //  return result
+
+    console.log(value.happy)
+
--- solution.js
+++ solution.js
@@ -5,6 +5,11 @@ function printNums(nums) {
    *  Using .forEach, add each number in the nums array to a new array.
    * @param nums => [1,2,3,4]
    */
+    newArray =[]
+    nums.forEach(element => {
+    newArray.push(element)
+  });
+  return newArray
 }
 
 function returnSums(nums) {
@@ -13,14 +18,28 @@ function returnSums(nums) {
    * to a new array and return the new array
    * @param nums => [1,2,3,4,5]
    */
+    newArray = []
+    nums.forEach((element, i) => {
+        let newNum = element + i;
+        newArray.push(newNum);
+  });
+  return newArray
 }
 
-function returnTotal(objs) {
+function returnTotal(objs) { 
   /**
    * Using forEach return a number that is the total sum of all numbers in the array of objects.
    * The key for each object will be n
    * @param objs => [{n:1}]
    */
+   const sumall = objs.map(item => item.n).reduce((a, b) => a + b, 0);
+   return sumall
+   
+  //  const totals = objs.forEach((item) => {
+  //   let sum = 0; 
+  //   sum += item.n;
+  //   return sum})
+  //   return totals
 }
 
 function printMoney(decimals) {
@@ -33,6 +52,9 @@ function printMoney(decimals) {
    * }
    * Hint: Not all decimals have two places, make sure to return each decimal with the proper decimal places. .toFixed may be useful :)
    */
+   const formatted = decimals.map(a => a.toFixed(2)).map(x => `$${x}`)
+   return formatted
+   // toFixed not working
 }
 
 function returnAllTrueValues(values) {
@@ -40,6 +62,8 @@ function returnAllTrueValues(values) {
    * @param values => [{name: 'Joe', happy:false}]
    * Using .filter, return an array of objects where happy equals true
    */
+   const result = values.filter(mood => mood.happy === true);
+   return result
 }
 
 function addTwo(nums) {
@@ -50,6 +74,10 @@ function addTwo(nums) {
    */
   //   3
   //   4 => 7
+  const reduction = nums.reduce((a, b) => {
+   return (a + 2) + b
+  }, 0)
+  return reduction
 }
 
 function joinStrings(strings) {
@@ -57,6 +85,10 @@ function joinStrings(strings) {
    * @param strings => ['g', 'f', 'z']
    * Using .reduce, return a new string containing each string from the strings array.
    */
+  const jumble = strings.reduce((a, b) => {
+    return a + b
+  }) 
+  return jumble
 }
 
 //Bonus
@@ -66,6 +98,10 @@ function sortObjectsByValue(objs) {
    * Using .sort, sort the array of objects by the value key in ascending order
    * .sort documentation https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
    */
+   const sorted = objs.sort((a, b) => {
+    return a.value - b.value;
+    });
+    return sorted
 }
 
 module.exports = {
