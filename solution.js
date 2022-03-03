0198af777fd5077887c836f8c2e527325f302496
pregan23 <pregan2bing@gmail.com>
2022-03-02 19:49:00 -0500

    4 passed

--- solution.js
+++ solution.js
@@ -1,13 +1,28 @@
 // You must RETURN your result in each function!
 
 function printNums(nums) {
+  const newArray = []
+  nums.forEach((element) => {
+    newArray.push(element);  
+  })
+  return newArray;
   /**
    *  Using .forEach, add each number in the nums array to a new array.
    * @param nums => [1,2,3,4]
+   * 
    */
 }
 
 function returnSums(nums) {
+  const newArray = []
+  // const index = nums.indexOf(n)
+  nums.forEach((n) => {
+    n += nums.indexOf(n) 
+    newArray.push(n);
+  }
+  )
+  return newArray;
+  
   /**
    * Using forEach, push the sum of n plus the index
    * to a new array and return the new array
@@ -16,7 +31,17 @@ function returnSums(nums) {
 }
 
 function returnTotal(objs) {
+    // const newArray = []
+    let sum = 0;
+    objs.forEach((n) => {
+      if (objs.n !== NaN) {
+      sum += objs.n;
+      }
+    })
+    return sum;
   /**
+
+   * 
    * Using forEach return a number that is the total sum of all numbers in the array of objects.
    * The key for each object will be n
    * @param objs => [{n:1}]
@@ -24,6 +49,12 @@ function returnTotal(objs) {
 }
 
 function printMoney(decimals) {
+  const cashMoney = decimals.map((num) => {
+    const dollarFormat = '$' + num.toFixed(2)
+    return dollarFormat
+  })
+  return cashMoney;
+
   /**
    * @param decimals => [2.30, 2.40, 32.99]
    * Using .map, return an array of decimals formatted as dollars.
@@ -36,6 +67,15 @@ function printMoney(decimals) {
 }
 
 function returnAllTrueValues(values) {
+
+  const happyPeople = values.filter((people) => {
+    if (people.happy === true) {
+      return people;
+    }
+
+    
+  })
+  return happyPeople;
   /**
    * @param values => [{name: 'Joe', happy:false}]
    * Using .filter, return an array of objects where happy equals true
