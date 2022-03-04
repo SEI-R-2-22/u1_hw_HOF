861aa2ffa22060da200a6d160fe330827a10e3da
Michelle Flores <lmfg2456@gmail.com>
2022-03-04 10:51:33 -0800

    HOF homwork

--- solution.js
+++ solution.js
@@ -1,47 +1,137 @@
 // You must RETURN your result in each function!
-
+let genNums = [1,2,3,4]
 function printNums(nums) {
-  /**
+  
+    let  newArray = [] ;
+    nums.forEach((elementNums) => {
+        newArray.push(elementNums)
+
+
+    }) 
+/**
    *  Using .forEach, add each number in the nums array to a new array.
    * @param nums => [1,2,3,4]
    */
+//push 1,2,3,4(elements) from nums array into new array by using .forEach
+//create new array
+return newArray;
 }
-
-function returnSums(nums) {
+printNums(genNums)
+//////////
+const nums1 = [1,2,3,4,5]
+function returnSums(nums1) {
   /**
    * Using forEach, push the sum of n plus the index
    * to a new array and return the new array
    * @param nums => [1,2,3,4,5]
    */
+      let nums2 = []
+      nums1.forEach ((n, i)=>{
+          nums2.push (n+i )
+
+
+      })
+        // console.log (nums2);
+        return nums2; 
 }
+ returnSums(nums1)
+
+
+
+/////////////////////////////////////
+////NUMBER 3//////
+// let obj1= {
+//   n:1
+// }
+// let obj2 = {
+//   n:2
+
+// }
+// let obj3 ={
+//   n:3
+// }
+// let objArr= [obj1, obj2,obj3]
+
 
 function returnTotal(objs) {
+    let objArr = []
+    objs.forEach ((allObjects) => {
+      objArr.push(allObjects.n)
+
+
+    } )
+    return objArr.reduce( (newObjects, ob) => {
+    return newObjects + ob
+    } )
+}
   /**
    * Using forEach return a number that is the total sum of all numbers in the array of objects.
    * The key for each object will be n
    * @param objs => [{n:1}]
-   */
+// use for each
+//---pull info from array
+// return a number = sum of all numbers in objects array
+
+
+
+//////////////////////////////////////////////////
+/////NUMBER 4/////
+/////////
+//
+  //  * Hint: Not all decimals have two places, make sure to return each decimal with the proper decimal places. .toFixed may be useful :)
+  //  */
+// 
+
+
+let oldMoney = [2.3088, 2.4065, 32.9978]
+function printMoney(decimals) { 
+  let moneyDecimals = decimals.map((money) => {
+      return "$" + money.toFixed(2)
+    
+  })
+  return moneyDecimals
 }
 
-function printMoney(decimals) {
-  /**
-   * @param decimals => [2.30, 2.40, 32.99]
-   * Using .map, return an array of decimals formatted as dollars.
-   * EX: {
-   * input: [12.34, 9.99],
-   * output: ['$12.34', '$9.99']
-   * }
-   * Hint: Not all decimals have two places, make sure to return each decimal with the proper decimal places. .toFixed may be useful :)
-   */
-}
 
-function returnAllTrueValues(values) {
+
+
+
+
+
+
+
+
+//pull info from array given using .map
+//format array as map
+// return array from decimals to dollars $(from number to string because)
+//return decimals in same place
+
+
+
+/////////////////
+////number 5///////
+////////
   /**
    * @param values => [{name: 'Joe', happy:false}]
    * Using .filter, return an array of objects where happy equals true
    */
-}
+  
+    // printMoney(theMoney)
 
+    let values = [{name: 'Joe', happy:false}] [{name: 'michelle', happy: true}]
+   function returnAllTrueValues(values) {
+    let truth = values.filter((honest) => {
+      if(honesty.happy === true) {
+        return honesty;
+    }
+      return truth
+    }
+  }
+
+
+//use .filter to return an array with true if name is happy
+//true is a boolean
+///////////////////////////
 function addTwo(nums) {
   /**
    * @param nums => [1,2,3,4]
